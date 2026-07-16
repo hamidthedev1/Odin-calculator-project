8fa3d64b0bdd5fb2f01956f9a46fe7e22a0bda9d
Hamid Ahmed <hamidabuhayat1@gmail.com>
2026-07-16 06:13:28 +0300

    feat: create operate function with three parameter

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ return num1 * num2;
 function divide(num1, num2){
 return num1 / num2;
 }
+
+// create operate function
+
+function operate (operator, firstNumber, secondNumber) {
+    switch(operator) {
+        case "+":
+         return add(firstNumber, secondNumber);
+        case "-":
+         return subtract(firstNumber, secondNumber);
+        case "*":
+         return multiply(firstNumber, secondNumber);
+        case "/":
+         return divide(firstNumber, secondNumber);
+
+    default:
+        return "Invalid Operator"
+    }
+}
